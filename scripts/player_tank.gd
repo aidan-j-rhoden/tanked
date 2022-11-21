46bffcb77ebceec56b97446db67d550601c0d491
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2022-11-21 11:09:47 -0600

    WE NOW HAVE MULTIPLAYER!

--- scripts/player_tank.gd
+++ scripts/player_tank.gd
@@ -3,7 +3,7 @@ extends "res://scripts/base_tank.gd"
 # Get input from player
 func get_controls():
 	rot_dir = 0
-	velocity = Vector2(0,0)
+	velocity = Vector2(0, 0)
 
 	if Input.is_action_pressed("left_%s" % my_id):
 		rot_dir = -1
